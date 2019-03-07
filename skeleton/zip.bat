fd9041535819532187129e123cfb75fd553ff4ef
Lovac42 <lovac42@protonmail.com>
2019-03-07 02:10:11 -0500

    updated zip

--- skeleton/zip.bat
+++ skeleton/zip.bat
@@ -2,7 +2,12 @@
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=addonManTest
 
-%ZIP% %REPO%_20.zip *.py %REPO%/*.*
+fsum -r -jm -md5 -d%REPO% * > checksum.md5
+move checksum.md5 %REPO%/checksum.md5
+
+quick_manifest.exe "addon_test" "%REPO%" >%REPO%/manifest.json
+
+%ZIP% %REPO%_20.zip *.py %REPO%/*
 
 cd %REPO%
-%ZIP% ../%REPO%_21.zip *.*
+%ZIP% ../%REPO%_21.ankiaddon *
